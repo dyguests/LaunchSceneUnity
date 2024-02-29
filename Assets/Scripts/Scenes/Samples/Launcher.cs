b15773976cb5a0f3206c33ece20485b16c5422f4
fanhl <dyguests@qq.com>
2024-02-29 09:58:22 +0800

    unity->Android

--- Assets/Scripts/Scenes/Samples/Launcher.cs
+++ Assets/Scripts/Scenes/Samples/Launcher.cs
@@ -5,9 +5,18 @@ namespace Scenes.Samples
 {
     public static class Launcher
     {
+        // ReSharper disable once InconsistentNamingRE
+        // ReSharper disable once InconsistentNaming
+        private static AndroidJavaClass _LauncherJava;
+
+        // ReSharper disable once InconsistentNaming
+        private static AndroidJavaClass LauncherJava => _LauncherJava ??= new AndroidJavaClass("com.fanhl.launchsceneandroid.sdk.Launcher");
+
+
         public static void LaunchBActivity()
         {
             Debug.Log("Launcher LaunchBActivity");
+            LauncherJava.Call("launchB", UnityPlayerAndroid.CurrentActivity);
         }
     }
 }
---
+++ Assets/Scripts/Scenes/Samples/UnityPlayerAndroid.cs
@@ -0,0 +1,31 @@
+﻿#if UNITY_ANDROID
+using System;
+using UnityEngine;
+
+namespace Scenes.Samples
+{
+    internal static class UnityPlayerAndroid
+    {
+        // ReSharper disable once InconsistentNaming
+        private static AndroidJavaClass _UnityPlayerJava;
+        private static AndroidJavaClass UnityPlayerJava => _UnityPlayerJava ??= new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+
+        public static AndroidJavaObject CurrentActivity
+        {
+            get
+            {
+                // var unityPlayerJava = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+                // Log.N($"unityPlayerJava:{unityPlayerJava}");
+                var unityActivity = UnityPlayerJava.GetStatic<AndroidJavaObject>("currentActivity");
+                // Log.N($"unityActivity:{unityActivity}");
+                return unityActivity;
+            }
+        }
+
+        public static void RunOnUiThread(Action action)
+        {
+            CurrentActivity.Call("runOnUiThread", new AndroidJavaRunnable(action.Invoke));
+        }
+    }
+}
+#endif
